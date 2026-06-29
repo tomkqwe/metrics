7e4b991414589dc895eb4569b90b1975adeb0b6b
avlebedev <ar.lebedev@postgrespro.ru>
2026-06-29 22:08:30 +0300

    [iter4] add cmd flags to main

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/tomkqwe/metrics/internal/agent"
@@ -11,13 +14,24 @@ import (
 )
 
 const (
-	serverURL      = "http://localhost:8080"
-	pollInterval   = 2 * time.Second
-	reportInterval = 10 * time.Second
+	defaultServerAddress         = "localhost:8080"
+	defaultPollIntervalSeconds   = 2
+	defaultReportIntervalSeconds = 10
 )
 
+type config struct {
+	serverAddress  string
+	pollInterval   time.Duration
+	reportInterval time.Duration
+}
+
 func main() {
-	app, err := newAgent()
+	cfg, err := parseConfig(os.Args[1:])
+	if err != nil {
+		os.Exit(1)
+	}
+
+	app, err := newAgent(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,12 +39,41 @@ func main() {
 	app.Run()
 }
 
-func newAgent() (*agent.Agent, error) {
+func parseConfig(args []string) (config, error) {
+	var cfg config
+	var pollIntervalSeconds int
+	var reportIntervalSeconds int
+
+	flags := flag.NewFlagSet("agent", flag.ContinueOnError)
+	flags.SetOutput(os.Stderr)
+	flags.StringVar(&cfg.serverAddress, "a", defaultServerAddress, "HTTP server address")
+	flags.IntVar(&reportIntervalSeconds, "r", defaultReportIntervalSeconds, "metrics report interval in seconds")
+	flags.IntVar(&pollIntervalSeconds, "p", defaultPollIntervalSeconds, "metrics poll interval in seconds")
+
+	if err := flags.Parse(args); err != nil {
+		return config{}, err
+	}
+
+	cfg.pollInterval = time.Duration(pollIntervalSeconds) * time.Second
+	cfg.reportInterval = time.Duration(reportIntervalSeconds) * time.Second
+
+	return cfg, nil
+}
+
+func newAgent(cfg config) (*agent.Agent, error) {
 	return agent.NewAgent(
 		collector.NewRuntimeCollector(),
 		storage.NewMemoryStorage(),
-		sender.NewHTTPSender(serverURL),
-		pollInterval,
-		reportInterval,
+		sender.NewHTTPSender(serverURL(cfg.serverAddress)),
+		cfg.pollInterval,
+		cfg.reportInterval,
 	)
+}
+
+func serverURL(address string) string {
+	if strings.HasPrefix(address, "http://") || strings.HasPrefix(address, "https://") {
+		return address
+	}
+
+	return "http://" + address
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/tomkqwe/metrics/internal/handler"
@@ -9,14 +11,39 @@ import (
 	"github.com/tomkqwe/metrics/internal/service"
 )
 
+const defaultServerAddress = "localhost:8080"
+
+type config struct {
+	serverAddress string
+}
+
 func main() {
+	cfg, err := parseConfig(os.Args[1:])
+	if err != nil {
+		os.Exit(1)
+	}
+
 	handler, err := newServerHandler()
 	if err != nil {
 		panic(err)
 	}
-	if err := http.ListenAndServe(":8080", handler); err != nil {
+	if err := http.ListenAndServe(cfg.serverAddress, handler); err != nil {
 		panic(err)
 	}
+}
+
+func parseConfig(args []string) (config, error) {
+	var cfg config
+
+	flags := flag.NewFlagSet("server", flag.ContinueOnError)
+	flags.SetOutput(os.Stderr)
+	flags.StringVar(&cfg.serverAddress, "a", defaultServerAddress, "HTTP server address")
+
+	if err := flags.Parse(args); err != nil {
+		return config{}, err
+	}
+
+	return cfg, nil
 }
 
 func newServerHandler() (http.Handler, error) {
